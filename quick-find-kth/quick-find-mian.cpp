fda9c8ccc3324355bd788c55df3f5c852fb91252
肖杰 <xiaojie@sansi.com>
2021-05-14 08:20:26 +0800

    feat(sort):implement bubble sort,insert sort ,quick sort

---
+++ quick-find-kth/quick-find-mian.cpp
@@ -0,0 +1,90 @@
+
+#include <iostream>
+
+#include <vector>
+
+using namespace std;
+
+
+int partition(vector<int> &a, int left, int right)
+{
+	int key = a[left];
+	while (left < right) {
+		while ((left < right) && (a[right] <= key))
+			right--;
+		a[left] = a[right];
+		while (left < right && a[left] >= key)
+			left++;
+		a[right] = a[left];
+	}
+	a[left] = key;
+	return left;
+}
+
+void printVec(const vector<int> &a, int index)
+{
+	const int temp = a[index];
+	cout << "before " << index << endl;
+	for (int i = 0; i < a.size() && i<=index; i++) {
+		cout << " a[" << i << "]= ";
+		if (temp >= a[i]) {
+			cout<< a[i];
+		}
+		else {
+			cout << "error";
+		}
+	}
+	cout << endl;
+
+	cout << "after " << index << endl;
+	for (int i = index+1; i < a.size(); i++) {
+		cout << " a[" << i << "]= ";
+		if (temp <= a[i]) {
+			cout << a[i];
+		}
+		else {
+			cout << "error";
+		}
+	}
+	cout << endl;
+}
+
+int quickFind(vector<int> &a, int left, int right, int k)
+{
+	
+	if (left == right) {
+		return -1;
+	}
+
+	int index = partition(a, left, right);
+	printVec(a, index);
+	cout << "index= " << index << " k= " << k << endl;
+	cout << "left= " << left << " right= " << right << endl;
+	if (index == (k - 1))
+		return a[index];
+	else if (index < k-1)
+		return quickFind(a, index + 1, right, k);
+	else
+		return quickFind(a, left, index - 1, k);
+	//quickFind(a, index + 1, right, k);
+	//quickFind(a, left, index - 1, k);
+}
+
+int findKth(vector<int> &a, int n, int K) 
+{
+	// write code here
+	return quickFind(a, 0, n - 1, K);
+}
+
+int main(int argc, char** argv)
+{
+	vector<int> a = { 1332802,1177178,1514891,871248,753214,123866,1615405,328656,1540395,
+		968891,1884022,252932,1034406,1455178,821713,486232,860175,1896237,852300,566715,
+		1285209,1845742,883142,259266,520911,1844960,218188,1528217,332380,261485,1111670,
+		16920,1249664,1199799,1959818,1546744,1904944,51047,1176397,190970,48715,349690,673887,
+		1648782,1010556,1165786,937247,986578,798663 };
+	cout << "a.size=" << a.size() << endl;
+	cout << findKth(a, 49, 24) << endl;
+	return 0;
+
+}
---
+++ sort/sort-main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+void swapNum(int &a, int &b)
+{
+	int temp = a;
+	a = b;
+	b = temp;
+}
+
+void printVector(const std::vector<int> &a, const std::string &str)
+{
+	std::cout << str << std::endl;
+	for (int i = 0; i < a.size(); i++) {
+		std::cout << " a[" << i << "]=" << a[i];
+	}
+	std::cout << std::endl;
+}
+
+
+void bubbleSort(std::vector<int> &a)
+{
+	for (int i = 0; i < a.size() - 1; i++) {
+		for (int j = 0; j < a.size() - 1 - i; j++) {
+			if (a[j] > a[j + 1]) {
+				swapNum(a[j], a[j + 1]);
+			}
+		}
+	}
+}
+
+void insertSort(std::vector<int> &a)
+{
+	for (int i = 1; i < a.size(); i++) {
+		int temp = a[i];
+		int j = i - 1;
+		while (j >= 0 && a[j] > temp) {
+			a[j + 1] = a[j];
+			j--;
+		}
+		a[j + 1] = temp;
+	}
+}
+
+int partition(std::vector<int>& a, int left, int right)
+{
+	if (left < 0 || right >= a.size()) {
+		return -1;
+	}
+	int key = a[left];
+	while (left < right) {
+		while (left < right && a[right] >= key) {
+			right--;
+		}
+		a[left] = a[right];
+		while (left < right && a[left] <= key) {
+			left++;
+		}
+		a[right] = a[left];
+	}
+	a[left] = key;
+	return left;
+}
+
+void quickSort(std::vector<int>& a, int left, int right)
+{
+	if (left >= right) {
+		return;
+	}
+
+	int index = partition(a, left, right);
+	quickSort(a, left, index - 1);
+	quickSort(a, index + 1, right);
+}
+
+int main(int argc, char **argv)
+{
+	std::vector<int> a = { 0,1,2,5,4,3,9,8,6,7 };
+
+	//printVector(a, "Before bubble sort:");
+	//bubbleSort(a);
+	//printVector(a, "After buble sort");
+
+	//printVector(a, "Before insert sort:");
+	//insertSort(a);
+	//printVector(a, "After insert sort");
+
+	printVector(a, "Before quick sort:");
+	quickSort(a, 0, a.size() - 1);
+	printVector(a, "After quick sort");
+	return 0;
+}
