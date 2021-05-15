7d34a6a87d7b54ea026a5ff5c3e682778ea8560c
肖杰 <xiaojie@sansi.com>
2021-05-15 13:31:11 +0800

    feat(queue):implement build queue with 2 stack

---
+++ build-queue-with-2stack/queue-with-2stack.cpp
@@ -0,0 +1,52 @@
+
+#include <iostream>;
+#include <stack>;
+
+static std::stack<int> stack1;
+static std::stack<int> stack2;
+static std::size_t queue_size = 0;
+
+
+static void push(int node)
+{
+	stack1.push(node);
+	queue_size++;
+}
+
+static int pop()
+{
+	if (!stack1.empty()) {
+		while (!stack1.empty()) {
+			stack2.push(stack1.top());
+			stack1.pop();
+		}
+	}
+	if (!stack2.empty()) {
+		int node = stack2.top();
+		queue_size--;
+		stack2.pop();
+		return node;
+	}
+	return -1;
+}
+
+int main(int argc, char **argv)
+{
+	int pushNum;
+	int temp;
+	std::cout << "Please input queue size: " << std::endl;
+	std::cin >> pushNum;
+
+	std::cout << "Please input queue member: " << std::endl;
+	for (int i = 0; i < pushNum; i++) {
+		std::cin >> temp;
+		push(temp);
+	}
+	
+	std::cout << "pop queue: " << std::endl;
+	for (int i = 0; i < pushNum; i++) {
+		std::cout << pop() << " ";
+	}
+	std::cout << std::endl;
+	return 0;
+}
