ef915cc91782f1c0765cc41f657dbe7613e4fcc0
肖杰 <xiaojie@sansi.com>
2021-06-01 20:03:38 +0800

    feat(big-number):implement big number multiply

--- big-number-sum/big-number-sum.cpp
+++ big-number-sum/big-number-sum.cpp
@@ -41,7 +41,12 @@ bool isValidNumberStr(const string &str)
 
 string getStrAbs(const string &str)
 {
-	return isSignedStr(str) ? str.substr(1) : str;
+	size_t index = isSignedStr(str) ? 1 : 0;
+	for (; index < str.length() - 1; index++) {
+		if (str[index] != '0')
+			break;
+	}
+	return str.substr(index);
 }
 
 //0=>a==b 1=>a>b -1=>a<b 
@@ -146,9 +151,52 @@ string subtract(const string &a, const string &b, const char resultSign)
 	return resultSign == '+' ? result.substr(index) : string("-").append(result.substr(index));
 }
 
+/***************************************************************************/
+const string multiplyChar(const string &a, const char ch, const int index)
+{
+	string result = "";
+	int carry = 0;
+
+	if (ch == '0')
+		return "0";
+
+	for (int i = 0; i < index; i++)
+		result.append("0");
+
+	for (int i = a.length() - 1; i >= 0; i--) {
+		int val = (a[i] - '0')*(ch - '0') + carry;
+		carry = val / 10;
+		val = val % 10;
+		result.append(to_string(val));
+	}
+
+	if (carry > 0)
+		result.append(to_string(carry));
+
+	reverse(result.begin(), result.end());
+
+	return result;
+}
+
+const string multiplyAbs(const string &a, const string &b)
+{
+	string result = "";
+	int index = 0;
+	for (int i = b.length() - 1; i >= 0; i--) {
+		const string temp = multiplyChar(a, b[i], index);
+		result = bigNumAdd(result, temp);
+		index++;
+	}
+	for (index = 0; index < result.length() - 1; index++) {
+		if (result[index] != '0') {
+			break;
+		}
+	}
+	return result.substr(index);
+}
 
 /***************************************************************************/
-const string bigNumSum(const string &lStr, const string &rStr)
+const string bigNumAdd(const string &lStr, const string &rStr)
 {
 	char lSign = '+';
 	char rSign = '+';
@@ -203,7 +251,7 @@ const string bigNumSubtract(const string &lStr, const string &rStr)
 	const string &&lAbs = getStrAbs(lStr);
 	const string &&rAbs = getStrAbs(rStr);
 	if (rSign == '-') {
-		return bigNumSum(lStr, rAbs);
+		return bigNumAdd(lStr, rAbs);
 	} else {
 		const int cmpFlag = compareAbs(lAbs, rAbs);
 		if (lSign == '-') {
@@ -218,6 +266,33 @@ const string bigNumSubtract(const string &lStr, const string &rStr)
 		}
 	}
 }
+
+const string bigNumMultiply(const string &lStr, const string &rStr)
+{
+	char lSign = '+';
+	char rSign = '+';
+	if (!isValidNumberStr(lStr))
+		return rStr;
+
+	if (!isValidNumberStr(rStr))
+		return lStr;
+
+	if (isSignedStr(lStr))
+		lSign = lStr[0];
+
+	if (isSignedStr(rStr))
+		rSign = rStr[0];
+
+	const string &&lAbs = getStrAbs(lStr);
+	const string &&rAbs = getStrAbs(rStr);
+	if (lAbs == "0" || rAbs == "0") 
+		return "0";
+	
+	const string &&absMultiplyValue = multiplyAbs(lAbs, rAbs);
+	
+	return lSign == rSign ? absMultiplyValue : string("-").append(absMultiplyValue);
+}
+
 /***************************************************************************/
 void testIsValidNumberStr(void)
 {
@@ -237,35 +312,35 @@ void testIsValidNumberStr(void)
 	cout << str << " is valid number string: " << isValidNumberStr(str) << endl;
 }
 
-void testBigNumberSum(void)
+void testBigNumberAdd(void)
 {
 	string a = "123456";
 	string b = "123456";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 
 	a = "9999";
 	b = "111";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 
 	a = "-9999";
 	b = "-111";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 
 	a = "-9666";
 	b = "999";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 
 	a = "111";
 	b = "-999";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 
 	a = "555";
 	b = "-44444";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 
 	a = "44444";
 	b = "-44440";
-	cout << a << " + " << b << " = " << bigNumSum(a, b) << endl;
+	cout << a << " + " << b << " = " << bigNumAdd(a, b) << endl;
 }
 
 
@@ -299,12 +374,31 @@ void testBigNumSubtract(void)
 	b = "-44440";
 	cout << a << " - " << b << " = " << bigNumSubtract(a, b) << endl;
 }
+
+void testBigNumMultiply(void)
+{
+	string a = "123456";
+	string b = "123456";
+	cout << a << " * " << b << " = " << bigNumMultiply(a,b) << endl;
+
+	a = "0";
+	cout << a << " * " << b << " = " << bigNumMultiply(a, b) << endl;
+
+	a = "54321";
+	b = "0";
+	cout << a << " * " << b << " = " << bigNumMultiply(a, b) << endl;
+
+	b = "-2";
+	cout << a << " * " << b << " = " << bigNumMultiply(a, b) << endl;
+}
 /***************************************************************************/
 int main(int argc, char **argv)
 {
 	//testIsValidNumberStr();
 
-	testBigNumberSum();
+	//testBigNumberAdd();
 
-	testBigNumSubtract();
+	//testBigNumSubtract();
+
+	testBigNumMultiply();
 }
