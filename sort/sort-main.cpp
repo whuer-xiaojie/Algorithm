9a31aff1c1e0d6b846845a88bf3d617afc1593cb
whuer-xiaojie <1939346428@qq.com>
2021-06-27 20:13:41 +0800

    feat(sort):implement heap sort

--- sort/sort-main.cpp
+++ sort/sort-main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 
 
 void swapNum(int &a, int &b)
@@ -117,10 +118,48 @@ void mergeSort(std::vector<int>& arr, int start, int end)
 		merge(arr, start, mid, mid + 1, end);
 	}
 }
+
+/*********************************************/
+void heapSort(std::vector<int>& arr)
+{
+	std::make_heap(arr.begin(), arr.end());
+	printVector(arr, "after make heap:");
+	std::sort_heap(arr.begin(), arr.end());
+}
+
+void max_heapify(int arr[], int start, int end)
+{
+	//建立父节点指标和子节点指标
+	int dad = start;
+	int son = dad * 2 + 1;
+	while (son <= end) { //若子节点指标在范围内才做比较
+		if (son + 1 <= end && arr[son] < arr[son + 1]) //先比较两个子节点大小，选择最大的
+			son++;
+		if (arr[dad] > arr[son]) //如果父节点大于子节点代表调整完毕，直接跳出函数
+			return;
+		else { //否则交换父子内容再继续子节点和孙节点比较
+			swapNum(arr[dad], arr[son]);
+			dad = son;
+			son = dad * 2 + 1;
+		}
+	}
+}
+void heap_sort(int arr[], int len)
+{
+	int i;
+	//初始化，i从最后一个父节点开始调整
+	for (i = len / 2 - 1; i >= 0; i--)
+		max_heapify(arr, i, len - 1);
+	//先将第一个元素和已排好元素前一位做交换，再从新调整，直到排序完毕
+	for (i = len - 1; i > 0; i--) {
+		swapNum(arr[0], arr[i]);
+		max_heapify(arr, 0, i - 1);
+	}
+}
 /*********************************************/
 int main(int argc, char **argv)
 {
-	std::vector<int> arr = { 0,1,2,5,4,3,9,8,6,7 };
+	std::vector<int> arr = { 0,1,1,2,5,4,3,9,8,6,7 };
 
 	//printVector(arr, "Before bubble sort:");
 	//bubbleSort(arr);
@@ -134,9 +173,14 @@ int main(int argc, char **argv)
 	//quickSort(arr, 0, arr.size() - 1);
 	//printVector(arr, "After quick sort");
 
-	printVector(arr, "Before quick sort:");
-	mergeSort(arr, 0, arr.size() - 1);
-	printVector(arr, "After quick sort");
+	//printVector(arr, "Before quick sort:");
+	//mergeSort(arr, 0, arr.size() - 1);
+	//printVector(arr, "After quick sort");
+
+	printVector(arr, "Before heap sort:");
+	heapSort(arr);
+	printVector(arr, "After heap sort");
+
 	return 0;
 }
 
